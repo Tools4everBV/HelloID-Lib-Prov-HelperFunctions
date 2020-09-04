b2baf762d8507f5e8a5dd86879d5c5e1b89ce0d5
Mike Sheldon <m.sheldon@tools4ever.com>
2020-09-04 10:54:47 -0700

    Regex Fixes
    Regex Fixes by Rick Jongbloed

--- Javascript/Algorithms/sAMAccountName.birthname.js
+++ Javascript/Algorithms/sAMAccountName.birthname.js
@@ -1,35 +1,35 @@
 function generateSamAccountName() {
     const suffix = Iteration === 0 ? '' : Iteration;
     let maxAttributeLength = 20 - suffix.toString().length;
-     
+    
     let firstName = Person.Name.NickName;
     let middleName = Person.Name.FamilyNamePrefix;
     let lastName = Person.Name.FamilyName;
- 
+
     let sAMAccountName = firstName + '.';
     //Check if the middleName variable contains data
     if (typeof middleName !== 'undefined' && middleName) { sAMAccountName = sAMAccountName + middleName.replace(/\s+/g, '.') + '.' }
     sAMAccountName = sAMAccountName + lastName;
- 
+
     //Remove tailing ...
     sAMAccountName = sAMAccountName.replace(/\.+$/, "");
- 
+
     //Remove leading ...
     sAMAccountName = sAMAccountName.replace(/^\.+/, "");
- 
+
     //Convert to lower case
     sAMAccountName = sAMAccountName.toLowerCase();
- 
+
     //Remove diacritical chars
     sAMAccountName = deleteDiacriticalMarks(sAMAccountName);
-     
-    //Remove specific chars   
-    sAMAccountName = sAMAccountName.replace(/[^0-9a-zA-Z.]/g, '');
- 
-    //Shorten string to maxAttributeLength minus iteration length
+    
+	//Remove specific chars    
+    sAMAccountName = sAMAccountName.replace(/[^0-9a-zA-Z.']/g, '');
+
+	//Shorten string to maxAttributeLength minus iteration length
     sAMAccountName = sAMAccountName.trim().substring(0, maxAttributeLength) + suffix;
- 
+
     return sAMAccountName;
 }
- 
+
 generateSamAccountName();
--- Javascript/Algorithms/sAMAccountName.iteration.js
+++ Javascript/Algorithms/sAMAccountName.iteration.js
@@ -1,60 +1,60 @@
 function generateSamAccountName() {
     const suffix = Iteration === 0 ? '' : Iteration;
     let maxAttributeLength = 20 - suffix.toString().length;
-     
+    
     let firstNameChar = Person.Name.NickName.substring(0, 1);
     let firstName = Person.Name.NickName;
     let middleName = Person.Name.FamilyNamePrefix;
     let lastName = Person.Name.FamilyName;
-     
+    
     let sAMAccountName = "";
-     
+    
     if (Iteration == 0)
     {
         sAMAccountName = firstName + '.';
         //Check if the middleName variable contains data
         if (typeof middleName !== 'undefined' && middleName) { sAMAccountName = sAMAccountName + middleName.replace(/\s+/g, '.') + '.' }
         sAMAccountName = sAMAccountName + lastName;
-         
+        
         sAMAccountName = sAMAccountName.trim().substring(0, 20);
     }
-     
+    
     if (Iteration == 1)
     {
         sAMAccountName = firstNameChar + '.';
         //Check if the middleName variable contains data
         if (typeof middleName !== 'undefined' && middleName) { sAMAccountName = sAMAccountName + middleName.replace(/\s+/g, '.') + '.' }
         sAMAccountName = sAMAccountName + lastName;
-         
+        
         sAMAccountName = sAMAccountName.trim().substring(0, 20);
     }
-     
+    
     if (Iteration >= 2)
     {
         sAMAccountName = firstNameChar + '.';
         //Check if the middleName variable contains data
         if (typeof middleName !== 'undefined' && middleName) { sAMAccountName = sAMAccountName + middleName.replace(/\s+/g, '.') + '.' }
         sAMAccountName = sAMAccountName + lastName;
-         
+        
         sAMAccountName = sAMAccountName.trim().substring(0, maxAttributeLength) + suffix;
     }
-     
+    
     //Remove tailing ...
     sAMAccountName = sAMAccountName.replace(/\.+$/, "");
- 
+
     //Remove leading ...
     sAMAccountName = sAMAccountName.replace(/^\.+/, "");
- 
+
     //Convert to lower case
     sAMAccountName = sAMAccountName.toLowerCase();
- 
+
     //Remove diacritical chars
     sAMAccountName = deleteDiacriticalMarks(sAMAccountName);
- 
-    //Remove specific chars   
-    sAMAccountName = sAMAccountName.replace(/[^0-9a-zA-Z.]/g, '');
- 
+
+    //Remove specific chars    
+    sAMAccountName = sAMAccountName.replace(/[^0-9a-zA-Z.']/g, '');
+
   return sAMAccountName;
 }
- 
+
 generateSamAccountName();
