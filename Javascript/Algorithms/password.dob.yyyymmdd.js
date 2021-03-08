0b93d651ede37237da5f17ed9e69c05527605488
Mike Sheldon <m.sheldon@tools4ever.com>
2021-03-08 13:20:59 -0800

    UTC Date fix

--- Javascript/Algorithms/password.dob.yyyymmdd.js
+++ Javascript/Algorithms/password.dob.yyyymmdd.js
@@ -1,9 +1,9 @@
 function getPassword() {
     let date = Person.Details.BirthDate;
 
-    let year = date.getFullYear();
-    let month = date.getMonth()+1;
-    let day = date.getDate();
+    let year = date.getUTCFullYear();
+    let month = date.getUTCMonth()+1;
+    let day = date.getUTCDate();
 
     if (day < 10) { day = '0' + day; }
     if (month < 10) { month = '0' + month; }
