4b66e2b8a7ea1901826f9423a352a293df9d0859
Niek de Melker <niekdemelker@live.nl>
2021-05-04 20:02:34 +0200

    Update initials.3char.js

--- Javascript/Algorithms/initials.3char.js
+++ Javascript/Algorithms/initials.3char.js
@@ -4,12 +4,13 @@ function formatInitials() {
      
     if(Person.Name.Initials && Person.Name.Initials !== "")
     {
-        initials = Person.Name.Initials
+        return Person.Name.Initials
+            // Remove all dots
             .replace(/([.])/g, '')
-            .substring(0, 3);
-
-        // Insert (.) after each char
-        return initials.replace(/(.{1})/g,"$1.");
+            // Take the first three characters
+            .substring(0, 3)
+            // Insert (.) after each char
+            .replace(/(.{1})/g,"$1.");
     }
 
     return initials;
