a63c5f74bf759ea773c1e3be4ccd2a7af4af5c5e
niekdemelker <niekdemelker@live.nl>
2021-05-04 17:12:01 +0200

    Update sAMAccountName.birthname.js

--- Javascript/Algorithms/sAMAccountName.birthname.js
+++ Javascript/Algorithms/sAMAccountName.birthname.js
@@ -1,35 +1,30 @@
 function generateSamAccountName() {
+
     const suffix = Iteration === 0 ? '' : Iteration;
     let maxAttributeLength = 20 - suffix.toString().length;
     
-    let firstName = Person.Name.NickName;
-    let middleName = Person.Name.FamilyNamePrefix;
-    let lastName = Person.Name.FamilyName;
+    let firstName = Person.Name.NickName.trim();
+    let middleName = Person.Name.FamilyNamePrefix.trim();
+    let lastName = Person.Name.FamilyName.trim();
 
-    let sAMAccountName = firstName + '.';
-    //Check if the middleName variable contains data
-    if (typeof middleName !== 'undefined' && middleName) { sAMAccountName = sAMAccountName + middleName.replace(/\s+/g, '.') + '.' }
-    sAMAccountName = sAMAccountName + lastName;
-
-    //Remove tailing ...
-    sAMAccountName = sAMAccountName.replace(/\.+$/, "");
-
-    //Remove leading ...
-    sAMAccountName = sAMAccountName.replace(/^\.+/, "");
-
-    //Convert to lower case
-    sAMAccountName = sAMAccountName.toLowerCase();
+    let sAMAccountName = [firstName, middleName, lastName]
+        // Filter empty values
+        .filter(function(x) {return x !== ""})
+        // Join values to single string
+        .join(' ')
+        //Change whitespaces to dots
+        .replace(/\s+/g, '.')
+        //Convert to lower case
+        .toLowerCase();
 
     //Remove diacritical chars
     sAMAccountName = deleteDiacriticalMarks(sAMAccountName);
     
-	//Remove specific chars    
+    //Remove specific chars    
     sAMAccountName = sAMAccountName.replace(/[^0-9a-zA-Z.']/g, '');
 
-	//Shorten string to maxAttributeLength minus iteration length
-    sAMAccountName = sAMAccountName.trim().substring(0, maxAttributeLength) + suffix;
-
-    return sAMAccountName;
+    //Shorten string to maxAttributeLength minus iteration length
+    return sAMAccountName.substring(0, maxAttributeLength) + suffix;
 }
 
 generateSamAccountName();
