e3d477a2fdfd9c7ef56b13e342eda69508cb0c3f
niekdemelker <niekdemelker@live.nl>
2021-05-04 16:41:11 +0200

    Update DisplayName.js
    - changed the formatting so it is more readable
    - fixes a problem with trailing -

--- Javascript/Algorithms/displayName.js
+++ Javascript/Algorithms/displayName.js
@@ -1,45 +1,32 @@
-function generatedisplayName() {
- 
+function generateDisplayName() {
+
     let firstName = Person.Name.NickName;
+    
     let middleName = Person.Name.FamilyNamePrefix;
     let lastName = Person.Name.FamilyName;
+    
     let middleNamePartner = Person.Name.FamilyNamePartnerPrefix;
     let lastNamePartner = Person.Name.FamilyNamePartner;
-     
+    
+    let birthName = [middleName, lastName].join(' ').trim();
+    let partnerName = [middleNamePartner, lastNamePartner].join(' ').trim();
+
     switch(Person.Name.Convention) {
-    case "B":
-        nameFormatted = firstName;
-        if (typeof middleName !== 'undefined' && middleName) { nameFormatted = nameFormatted + ' ' + middleName }
-        nameFormatted = nameFormatted + ' ' + lastName;
-        break;
-    case "P":
-        nameFormatted = firstName;
-        if (typeof middleNamePartner !== 'undefined' && middleNamePartner) { nameFormatted = nameFormatted + ' ' + middleNamePartner }
-        nameFormatted = nameFormatted + ' ' + lastNamePartner;
-        break;
-    case "BP":
-        nameFormatted = firstName;
-        if (typeof middleName !== 'undefined' && middleName) { nameFormatted = nameFormatted + ' ' + middleName }
-        nameFormatted = nameFormatted + ' ' + lastName + ' - ';
-        if (typeof middleNamePartner !== 'undefined' && middleNamePartner) { nameFormatted = nameFormatted + ' ' + middleNamePartner + ' ' }
-        nameFormatted = nameFormatted + lastNamePartner;
-        break;
-    case "PB":
-        nameFormatted = firstName;
-        if (typeof middleNamePartner !== 'undefined' && middleNamePartner) { nameFormatted = nameFormatted + ' ' + middleNamePartner }
-        nameFormatted = nameFormatted + ' ' + lastNamePartner + ' - ';
-        if (typeof middleName !== 'undefined' && middleName) { nameFormatted = nameFormatted + ' ' + middleName + ' ' }
-        nameFormatted = nameFormatted + lastName;
-        break;
-    default:
-        nameFormatted = firstName;
-        if (typeof middleName !== 'undefined' && middleName) { nameFormatted = nameFormatted + ' ' + middleName }
-        nameFormatted = nameFormatted + ' ' + lastName;
-        break;
+        case 'B':
+            return firstName + ' ' + birthName;
+        case 'BP':
+            return firstName + ' ' + [birthName, partnerName]
+                .filter(function(x) {return x !== ""})
+                .join(' - ');
+        case 'P':
+            return firstName + ' ' + partnerName;
+        case 'PB':
+            return firstName + ' ' + [partnerName, birthName]
+                .filter(function(x) {return x !== ""})
+                .join(' - ');
+        default:
+            return firstName + ' ' + birthName;
     }
-    const displayName = nameFormatted.trim();
- 
-return displayName;
 }
- 
-generatedisplayName();
+
+generateDisplayName();
