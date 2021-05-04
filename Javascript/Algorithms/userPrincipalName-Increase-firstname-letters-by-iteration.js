8f1900e7c0ce05a40db14876104175cf2f274c4a
niekdemelker <niekdemelker@live.nl>
2021-05-04 18:52:37 +0200

    Update userPrincipalName-Increase-firstname-letters-by-iteration.js

--- Javascript/Algorithms/userPrincipalName-Increase-firstname-letters-by-iteration.js
+++ Javascript/Algorithms/userPrincipalName-Increase-firstname-letters-by-iteration.js
@@ -12,33 +12,33 @@ function generateUserPrincipalName() {
 	
     const domain = 'domain.com';
        
-    let firstName = Person.Name.NickName;
-    let middleName = Person.Name.FamilyNamePrefix;
-    let lastName = Person.Name.FamilyName;
+    let firstName = Person.Name.NickName.trim();
+    let middleName = Person.Name.FamilyNamePrefix.trim();
+    let lastName = Person.Name.FamilyName.trim();
     
     if (Iteration < firstName.length) {
-        let userPrincipalName = firstName.substring(0,Iteration+1) + '.';
-        suffix = ''
+        let firstNameLetters = firstName.substring(0, Iteration + 1);
+        const suffix = '';
     } else {
-        let userPrincipalName = firstName.substring(0,1) + '.';
+        let firstNameLetters = firstName.substring(0, 1);
         const suffix = Iteration - firstName.length + 2;
     }
     
-    //Check if the middleName variable contains data
-    if (typeof middleName !== 'undefined' && middleName) { userPrincipalName = userPrincipalName + middleName.replace(/\s+/g, '.') + '.' }
-    userPrincipalName = userPrincipalName + lastName;
-    
-    //Remove blank chars
-    userPrincipalName = userPrincipalName.replace(/\s/g,'');
-    
-    //Convert to lower case
-    userPrincipalName = userPrincipalName.toLowerCase();
+    let userPrincipalName = [firstNameLetters, middleName, lastName]
+        // Filter empty values
+        .filter(function(x) {return x !== ""})
+        // Join values to single string
+        .join(' ')
+        //Change whitespaces to dots
+        .replace(/\s+/g, '.')
+        //Convert to lower case
+        .toLowerCase();
     
     //Remove diacritical chars
     userPrincipalName = deleteDiacriticalMarks(userPrincipalName);
 
 	//Remove specific chars    
-    userPrincipalName = userPrincipalName.trim().replace(/[^0-9a-zA-Z.']/g, '');
+    userPrincipalName = userPrincipalName.replace(/[^0-9a-zA-Z.']/g, '');
 
 	userPrincipalName = userPrincipalName + suffix + '@' + domain;
     
