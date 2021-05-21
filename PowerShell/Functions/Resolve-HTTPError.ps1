e149275164d66cdda9a7fa2c646547f7b42209f3
Jeroen Blaauw <72070984+JeroenBL@users.noreply.github.com>
2021-05-21 17:26:53 +0200

    Create Resolve-HTTPError.ps1

---
+++ PowerShell/Functions/Resolve-HTTPError.ps1
@@ -0,0 +1,39 @@
+function Resolve-HTTPError {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory,
+            ValueFromPipeline
+        )]
+        [object]$ErrorObject
+    )
+    process {
+        $HttpErrorObj = @{
+            FullyQualifiedErrorId = $ErrorObject.FullyQualifiedErrorId
+            InvocationInfo        = $ErrorObject.InvocationInfo.MyCommand
+            TargetObject          = $ErrorObject.TargetObject.RequestUri
+        }
+        if ($ErrorObject.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') {
+            $HttpErrorObj['ErrorMessage'] = $ErrorObject.ErrorDetails.Message
+        } elseif ($ErrorObject.Exception.GetType().FullName -eq 'System.Net.WebException') {
+            $stream = $ErrorObject.Exception.Response.GetResponseStream()
+            $stream.Position = 0
+            $streamReader = New-Object System.IO.StreamReader $Stream
+            $errorResponse = $StreamReader.ReadToEnd()
+            $HttpErrorObj['ErrorMessage'] = $errorResponse
+        }
+        Write-Output "'$($HttpErrorObj.ErrorMessage)', TargetObject: '$($HttpErrorObj.TargetObject), InvocationCommand: '$($HttpErrorObj.InvocationInfo)"
+    }
+}
+
+# Usage:
+try{
+
+} catch {
+    $ex = $PSItem
+    if ( $($ex.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') -or $($ex.Exception.GetType().FullName -eq 'System.Net.WebException')) {
+        $errorMessage = Resolve-HTTPError -Error $ex
+        Write-Verbose "Could not retrieve ... Error $errorMessage"
+    } else {
+        Write-Verbose "Could not retrieve ... Error: $($ex.Exception.Message)"
+    }
+}
