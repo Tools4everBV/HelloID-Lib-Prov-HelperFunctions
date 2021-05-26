583297b02fe796a2b821d2b57c8282af905dc4c9
Mike Sheldon <m.sheldon@tools4ever.com>
2021-05-26 06:06:28 -0700

    Revert "Update displayName.js"
    This reverts commit 962b1803f9d369284daae76014459ebdffc967ff.

--- Javascript/Algorithms/displayName.js
+++ Javascript/Algorithms/displayName.js
@@ -1,33 +1,29 @@
 function generateDisplayName() {
 
     let firstName = Person.Name.NickName.trim();
-
+    
     let middleName = Person.Name.FamilyNamePrefix.trim();
     let lastName = Person.Name.FamilyName.trim();
-
+    
     let middleNamePartner = Person.Name.FamilyNamePartnerPrefix.trim();
     let lastNamePartner = Person.Name.FamilyNamePartner.trim();
-
+    
     let birthName = [middleName, lastName].join(' ').trim();
     let partnerName = [middleNamePartner, lastNamePartner].join(' ').trim();
 
     switch(Person.Name.Convention) {
         case 'B':
             return firstName + ' ' + birthName;
-
         case 'BP':
             return firstName + ' ' + [birthName, partnerName]
                 .filter(function(x) {return x !== ""})
                 .join(' - ');
-
         case 'P':
             return firstName + ' ' + partnerName;
-
         case 'PB':
             return firstName + ' ' + [partnerName, birthName]
                 .filter(function(x) {return x !== ""})
                 .join(' - ');
-
         default:
             return firstName + ' ' + birthName;
     }
