c0a626bb2628c433f40b130d97af760f5f4333a6
Mike Sheldon <m.sheldon@tools4ever.com>
2021-05-26 06:07:25 -0700

    Revert "Update userPrincipalName.js"
    This reverts commit 68c002af9790ba3956d72826b58aac31c51f78d9.

--- Javascript/Algorithms/userPrincipalName.js
+++ Javascript/Algorithms/userPrincipalName.js
@@ -3,25 +3,26 @@ function generateUserPrincipalName() {
     const domain = 'yourdomainhere';
     const suffix = Iteration === 0 ? '' : Iteration;
     
-    let firstName = Person.Name.NickName.trim();
-    let middleName = Person.Name.FamilyNamePrefix.trim();
-    let lastName = Person.Name.FamilyName.trim();
+    let firstName = Person.Name.NickName;
+    let middleName = Person.Name.FamilyNamePrefix;
+    let lastName = Person.Name.FamilyName;
     
-    let userPrincipalName = [firstName, middleName, lastName]
-        // Filter empty values
-        .filter(function(x) {return x !== ""})
-        // Join values to single string
-        .join(' ')
-        //Change whitespaces to dots
-        .replace(/\s+/g, '.')
-        //Convert to lower case
-        .toLowerCase();
+    let userPrincipalName = firstName + '.';
+    //Check if the middleName variable contains data
+    if (typeof middleName !== 'undefined' && middleName) { userPrincipalName = userPrincipalName + middleName.replace(/\s+/g, '.') + '.' }
+    userPrincipalName = userPrincipalName + lastName;
+    
+    //Remove blank chars
+    userPrincipalName = userPrincipalName.replace(/\s/g,'');
+    
+    //Convert to lower case
+    userPrincipalName = userPrincipalName.toLowerCase();
     
     //Remove diacritical chars
     userPrincipalName = deleteDiacriticalMarks(userPrincipalName);
 
 	//Remove specific chars    
-    userPrincipalName = userPrincipalName.replace(/[^0-9a-zA-Z.']/g, '');
+    userPrincipalName = userPrincipalName.trim().replace(/[^0-9a-zA-Z.']/g, '');
 
 	userPrincipalName = userPrincipalName + suffix + '@' + domain;
     
