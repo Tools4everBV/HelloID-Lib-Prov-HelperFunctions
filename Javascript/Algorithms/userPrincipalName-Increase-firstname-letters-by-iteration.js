84830cef18eb9bcd6c6a5873fc842c31c54d6c9c
Rick Jongbloed <r.jongbloed@tools4ever.com>
2021-05-26 13:10:33 +0200

    Update userPrincipalName-Increase-firstname-letters-by-iteration.js
    Rolled back the original version as the change by @niekdemelker didn't work out of the box. @niekdemelker: Please test with null values on familyNamePrefix, partnerName and partnerNamePrefix before you enter a PR.

--- Javascript/Algorithms/userPrincipalName-Increase-firstname-letters-by-iteration.js
+++ Javascript/Algorithms/userPrincipalName-Increase-firstname-letters-by-iteration.js
@@ -12,36 +12,36 @@ function generateUserPrincipalName() {
 	
     const domain = 'domain.com';
        
-    let firstName = Person.Name.NickName.trim();
-    let middleName = Person.Name.FamilyNamePrefix.trim();
-    let lastName = Person.Name.FamilyName.trim();
-
+    let firstName = Person.Name.NickName;
+    let middleName = Person.Name.FamilyNamePrefix;
+    let lastName = Person.Name.FamilyName;
+    
     if (Iteration < firstName.length) {
-        let firstNameLetters = firstName.substring(0, Iteration + 1);
-        const suffix = '';
+        let userPrincipalName = firstName.substring(0,Iteration+1) + '.';
+        suffix = ''
     } else {
-        let firstNameLetters = firstName.substring(0, 1);
+        let userPrincipalName = firstName.substring(0,1) + '.';
         const suffix = Iteration - firstName.length + 2;
     }
-
-    let userPrincipalName = [firstNameLetters, middleName, lastName]
-        // Filter empty values
-        .filter(function(x) {return x !== ""})
-        // Join values to single string
-        .join(' ')
-        //Change whitespaces to dots
-        .replace(/\s+/g, '.')
-        //Convert to lower case
-        .toLowerCase();
-
+    
+    //Check if the middleName variable contains data
+    if (typeof middleName !== 'undefined' && middleName) { userPrincipalName = userPrincipalName + middleName.replace(/\s+/g, '.') + '.' }
+    userPrincipalName = userPrincipalName + lastName;
+    
+    //Remove blank chars
+    userPrincipalName = userPrincipalName.replace(/\s/g,'');
+    
+    //Convert to lower case
+    userPrincipalName = userPrincipalName.toLowerCase();
+    
     //Remove diacritical chars
     userPrincipalName = deleteDiacriticalMarks(userPrincipalName);
 
-    //Remove specific chars    
+	//Remove specific chars    
     userPrincipalName = userPrincipalName.replace(/[^0-9a-zA-Z.']/g, '');
 
-    userPrincipalName = userPrincipalName + suffix + '@' + domain;
-
+	userPrincipalName = userPrincipalName + suffix + '@' + domain;
+    
     return userPrincipalName;
 }
 
