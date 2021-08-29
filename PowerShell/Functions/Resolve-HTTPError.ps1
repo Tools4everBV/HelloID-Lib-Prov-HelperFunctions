00b3053ed96320d1f631c601e88b09ab7cee1234
Jeroen Blaauw <72070984+JeroenBL@users.noreply.github.com>
2021-08-29 21:57:11 +0200

    Update Resolve-HTTPError.ps1

--- PowerShell/Functions/Resolve-HTTPError.ps1
+++ PowerShell/Functions/Resolve-HTTPError.ps1
@@ -11,42 +11,55 @@ function Resolve-HTTPError {
         [object]$ErrorObject
     )
     process {
-        $HttpErrorObj = @{
+        $HttpErrorObj = [PSCustomObject]@{
             FullyQualifiedErrorId = $ErrorObject.FullyQualifiedErrorId
             MyCommand             = $ErrorObject.InvocationInfo.MyCommand
             RequestUri            = $ErrorObject.TargetObject.RequestUri
-            StackTrace            = $ErrorObject.ScriptStackTrace
+            ScriptStackTrace      = $ErrorObject.ScriptStackTrace
+            ErrorMessage          = ''
         }
         if ($ErrorObject.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') {
-            $HttpErrorObj['ErrorMessage'] = $ErrorObject.ErrorDetails.Message
+            $HttpErrorObj.ErrorMessage = $ErrorObject.ErrorDetails.Message
         } elseif ($ErrorObject.Exception.GetType().FullName -eq 'System.Net.WebException') {
             $stream = $ErrorObject.Exception.Response.GetResponseStream()
             $stream.Position = 0
             $streamReader = New-Object System.IO.StreamReader $Stream
             $errorResponse = $StreamReader.ReadToEnd()
-            $HttpErrorObj['ErrorMessage'] = $errorResponse
+            $HttpErrorObj.ErrorMessage = $errorResponse
         }
         Write-Output $HttpErrorObj
     }
 }
 
 # Usage in a target connector:
-# Prefered place to use this function (Resolve-HTTPError) is in the $dryRun and to let your functions (if any) just generate exceptions. This way, your errorMessages are 
-# as clean as possible. (The same goes for the StackTrace). Note that, in the example below, we don't only write a message for any error of type [HttpResponseException] 
+# Best practice is to let al your functions (if any) just generate exceptions. Create a single try/catch block and place the dryRun block inside the try. 
+# This way, your errorMessages are as clean as possible. (The same goes for the StackTrace). 
+# Note that, in the example below, we don't only write a message for any error of type [HttpResponseException] 
 # and [WebException] but also for anything else. 
-if(-not($dryRun -eq $true)){
-    try{
-
-    } catch {
-        $ex = $PSItem
-        if ( $($ex.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') -or $($ex.Exception.GetType().FullName -eq 'System.Net.WebException')) {
-            $errorObject = Resolve-HTTPError -Error $ex
-            Write-Verbose "Could not retrieve... Error $($errorObject.ErrorMessage)"
-        } else {
-            Write-Verbose "Could not retrieve... Error: $($ex.Exception.Message)"
-        }
+try{
+    if (-not ($dryRun -eq $true)){
+        # Create logic here
+    }
+} catch {
+    $ex = $PSItem
+    if ( $($ex.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') -or $($ex.Exception.GetType().FullName -eq 'System.Net.WebException')) {
+        $errorObject = Resolve-HTTPError -Error $ex
+        Write-Verbose "Could not create account. Error $($errorObject.ErrorMessage)"
+    } else {
+        Write-Verbose "Could not create account. Error: $($ex.Exception.Message)"
     }
 }
 
 # Usage in a source connector:
 # For a source connector, you can copy and paste the same code block and use it in the code where you retrieve the persons/departments.
+try {
+    # persons / departments logic here
+} catch {
+    $ex = $PSItem
+    if ( $($ex.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') -or $($ex.Exception.GetType().FullName -eq 'System.Net.WebException')) {
+        $errorObject = Resolve-HTTPError -Error $ex
+        Write-Verbose "Could not retrieve... Error $($errorObject.ErrorMessage)"
+    } else {
+        Write-Verbose "Could not retrieve... Error: $($ex.Exception.Message)"
+    }
+}
