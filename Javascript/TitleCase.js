3f2446ce7531d8246e606d20f20c8681502e4b28
Mike Sheldon <m.sheldon@tools4ever.com>
2021-12-08 14:27:14 -0800

    Update TitleCase.js
    Updated to handle spaces and hypens

--- Javascript/TitleCase.js
+++ Javascript/TitleCase.js
@@ -1,7 +1,5 @@
 function titleCase(str) {
-  str = str.toLowerCase().split(' ');
-  for (var i = 0; i < str.length; i++) {
-    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
-  }
-  return str.join(' ');
+  return str.toLowerCase().replace(/(?:^|[\s-/])\w/g, function (match) {
+        return match.toUpperCase();
+    });
 }
