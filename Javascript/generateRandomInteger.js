3bc4da2f353c01f160fa6c227b9f2a0e4aca1694
Grady Koopman <g.koopman@tools4ever.com>
2022-01-17 16:18:22 +0100

    Create generateRandomInteger.js
    This function returns a random generated number between min (included) and max (excluded) value

---
+++ Javascript/generateRandomInteger.js
@@ -0,0 +1,7 @@
+function generateRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+generateRandomInteger(1000, 9999);
+
+// Use the following solution to use the generated value as a string
+//generateRandomInteger.(1000, 9999).toString();
