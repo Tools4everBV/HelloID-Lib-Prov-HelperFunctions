654ec0abe76419293a94aed7d371e46b590c82d3
rschouten97 <69046642+rschouten97@users.noreply.github.com>
2022-07-27 11:15:39 +0200

    Updated with example to prefix if not X characters long

--- Javascript/generateRandomInteger.js
+++ Javascript/generateRandomInteger.js
@@ -4,4 +4,7 @@ function generateRandomInteger(min, max) {
 generateRandomInteger(1000, 9999);
 
 // Use the following solution to use the generated value as a string
-//generateRandomInteger.(1000, 9999).toString();
+//generateRandomInteger(1000, 9999).toString();
+
+// Use the following Make sure random integer is 2 characters long, if not, prefix with '0'
+// ('000000' + generateRandomInteger(1000, 9999).toString()).slice(-6);
