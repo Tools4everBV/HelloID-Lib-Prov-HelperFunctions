bd7ef9b70d4f548773bdb4a6adfe292ccc13552d
Mike Sheldon <m.sheldon@tools4ever.com>
2022-08-09 08:39:41 -0800

    Update removeDiacriticMarks.js
    Added Curly Quotes replacement

--- Javascript/removeDiacriticMarks.js
+++ Javascript/removeDiacriticMarks.js
@@ -1,3 +1,4 @@
 function removeDiacriticMarks(str) {
-  return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  //Replace Diacritic Marks and Curly Quotes
+  return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/[\u2018\u2019]/g, "'");
 }
