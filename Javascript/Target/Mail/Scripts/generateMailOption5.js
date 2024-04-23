e45b9b3a96cae0495e25cb30497bb376e561ded1
rschouten97 <69046642+rschouten97@users.noreply.github.com>
2024-04-23 13:07:05 +0200

    Improved readability and ease of use; included fixes from implementation

--- Javascript/Target/Mail/Scripts/generateMailOption5.js
+++ Javascript/Target/Mail/Scripts/generateMailOption5.js
@@ -1,52 +1,39 @@
-// generateMailOption5.js [https://github.com/Tools4everBV/HelloID-Lib-Prov-HelperFunctions/blob/master/Javascript/Target/Mail/README.md]
+// generateMailOption5.js [https://github.com/Tools4everBV/HelloID-Lib-Prov-HelperFunctions/blob/feat-rework-javascript/Javascript/Target/Mail/Scripts/generateMailOption5.js]
 //
 // Mapping logic to generate the Mail according to the following convention.
-// Eerste keuze	            B	jvandenboele@domain.local
+// First choice	            B	jvandenboele@domain.local
 // 	                        BP	jvandenboele@domain.local
 // 	                        P	jdevries@domain.local
 // 	                        PB  jdevries@domain.local
-// Indien in gebruik	    B	javandenboele@domain.local
+// If in use	            B	javandenboele@domain.local
 // 	                        BP	javandenboele@domain.local
 // 	                        P	jadevries@domain.local
 // 	                        PB	jadevries@domain.local
-// Indien ook in gebruik	B	janvandenboele@domain.local
+// If also in use	        B	janvandenboele@domain.local
 // 	                        BP	janvandenboele@domain.local
 // 	                        P	jandevries@domain.local
 // 	                        PB	jandevries@domain.local
+// etc.
 function generateMail() {
-    let firstName = Person.Name.NickName;
+    let nickName = Person.Name.NickName;
     let middleName = Person.Name.FamilyNamePrefix;
     let lastName = Person.Name.FamilyName;
-    let middleNamePartner = Person.Name.FamilyNamePartnerPrefix;
-    let lastNamePartner = Person.Name.FamilyNamePartner;
     let convention = Person.Name.Convention;
 
-    const domain = 'domain.local';
-
-    let suffix = '';
-    let nameFormatted = firstName.substring(0, (Iteration + 1));
-    if (Iteration > (firstName.length - 1)) {
-        suffix = (Iteration - (firstName.length - 2));
-    }
-
-    let maxAttributeLength = (256 - suffix.toString().length - domain.toString().length);
+    let mailNickName = nickName.substring(0, (Iteration + 1));
 
     switch (convention) {
-
         case "P":
         case "PB":
-            if (typeof middleNamePartner !== 'undefined' && middleNamePartner) { nameFormatted = nameFormatted + middleNamePartner }
-            nameFormatted = nameFormatted + lastNamePartner;
-            break;
         case "B":
         case "BP":
         default:
-            if (typeof middleName !== 'undefined' && middleName) { nameFormatted = nameFormatted + middleName }
-            nameFormatted = nameFormatted + lastName;
+            if (typeof middleName !== 'undefined' && middleName) { mailNickName = mailNickName + middleName.replace(/ /g, '') }
+            mailNickName = mailNickName + lastName;
             break;
     }
     // Trim spaces at start and end
-    let mailNickName = nameFormatted.trim();
+    mailNickName = mailNickName.trim();
 
     // Convert to lower case
     mailNickName = mailNickName.toLowerCase();
@@ -55,13 +42,20 @@ function generateMail() {
     mailNickName = deleteDiacriticalMarks(mailNickName);
 
     // Remove blank chars and "'"
-    mailNickName = mailNickName.replace(/[^0-9a-zA-Z.-_]/g, '');
+    mailNickName = mailNickName.replace(/[^0-9a-zA-Z.\-_]/g, '');
 
     // Shorten string to maxAttributeLength minus iteration length
+    let suffix = ''
+    let iterationToUse = Iteration - (nickName.length - 2)
+    suffix = Iteration === 0 ? '' : (iterationToUse);
+    const domain = 'domain.local';
+    const maxAttributeLength = (256 - suffix.toString().length - domain.toString().length);
     mailNickName = mailNickName.substring(0, maxAttributeLength);
 
     // Use the iterator if needed
-    mailNickName = mailNickName + suffix;
+    if (Iteration > (nickName.length - 1)) {
+        mailNickName = mailNickName + suffix;
+    }
 
     return mailNickName + '@' + domain;
 }
