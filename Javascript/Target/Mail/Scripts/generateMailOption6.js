a8346d53360d5e092087e46485b26e80b6fbf9b8
rschouten97 <69046642+rschouten97@users.noreply.github.com>
2024-04-23 13:01:56 +0200

    Improved readability and ease of use; included fixes from implementation

--- Javascript/Target/Mail/Scripts/generateMailOption6.js
+++ Javascript/Target/Mail/Scripts/generateMailOption6.js
@@ -1,4 +1,4 @@
-// generateMailOption6.js [https://github.com/Tools4everBV/HelloID-Lib-Prov-HelperFunctions/blob/feat-rework-javascript/Javascript/Target/Mail/Scripts/generateMailOption6.js]
+// generateMailOption7.js [https://github.com/Tools4everBV/HelloID-Lib-Prov-HelperFunctions/blob/feat-rework-javascript/Javascript/Target/Mail/Scripts/generateMailOption7.js]
 //
 // Mapping logic to generate the Mail according to the following convention.
 // First choice	        B	janine.vandenboele@domain.local
@@ -17,28 +17,23 @@
 // 	                    BP	j.vandenboele2@domain.local
 // 	                    P	j.vandenboele2@domain.local
 // 	                    PB  j.vandenboele2@domain.local
+// etc.
 function generateMail() {
-    let firstName = Person.Name.NickName;
+    let nickName = Person.Name.NickName;
     let middleName = Person.Name.FamilyNamePrefix;
     let lastName = Person.Name.FamilyName;
     let convention = Person.Name.Convention;
 
-    const domain = 'domain.local';
-
-    let suffix = '';
-    let nameFormatted = '';
+    let mailNickName = '';
     if (Iteration === 0) {
-        nameFormatted = firstName + '.';
+        mailNickName = nickName + '.';
     } else if (Iteration === 1) {
-        nameFormatted = firstName + '_';
+        mailNickName = nickName + '_';
     } else if (Iteration === 2) {
-        nameFormatted = firstName.substring(0, 1) + '.';
+        mailNickName = nickName.substring(0, 1) + '.';
     } else {
-        nameFormatted = firstName.substring(0, 1) + '.';
-        suffix = Iteration - 1;
+        mailNickName = nickName.substring(0, 1) + '.';
     }
-
-    let maxAttributeLength = (256 - suffix.toString().length - domain.toString().length);
 
     switch (convention) {
         case "P":
@@ -46,12 +41,12 @@ function generateMail() {
         case "B":
         case "BP":
         default:
-            if (typeof middleName !== 'undefined' && middleName) { nameFormatted = nameFormatted + middleName }
-            nameFormatted = nameFormatted + lastName;
+            if (typeof middleName !== 'undefined' && middleName) { mailNickName = mailNickName + middleName.replace(/ /g, '') }
+            mailNickName = mailNickName + lastName;
             break;
     }
     // Trim spaces at start and end
-    let mailNickName = nameFormatted.trim();
+    mailNickName = mailNickName.trim();
 
     // Convert to lower case
     mailNickName = mailNickName.toLowerCase();
@@ -60,9 +55,14 @@ function generateMail() {
     mailNickName = deleteDiacriticalMarks(mailNickName);
 
     // Remove blank chars and "'"
-    mailNickName = mailNickName.replace(/[^0-9a-zA-Z.-_]/g, '');
+    mailNickName = mailNickName.replace(/[^0-9a-zA-Z.\-_]/g, '');
 
     // Shorten string to maxAttributeLength minus iteration length
+    let suffix = ''
+    let iterationToUse = Iteration - 1
+    suffix = Iteration === 0 ? '' : (iterationToUse);
+    const domain = 'domain.local';
+    const maxAttributeLength = (256 - suffix.toString().length - domain.toString().length);
     mailNickName = mailNickName.substring(0, maxAttributeLength);
 
     // Use the iterator if needed
