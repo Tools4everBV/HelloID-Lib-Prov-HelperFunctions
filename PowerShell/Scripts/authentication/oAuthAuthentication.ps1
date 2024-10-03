cd32f044b90a761ab04c53f397b1dfdd77073895
Jeroen Blaauw <72070984+JeroenBL@users.noreply.github.com>
2024-10-03 08:52:24 +0200

    Update oAuthAuthentication.ps1 (#23)

--- PowerShell/Scripts/authentication/oAuthAuthentication.ps1
+++ PowerShell/Scripts/authentication/oAuthAuthentication.ps1
@@ -1,13 +1,18 @@
 # OAuth 2 authentication example
-# ---------------------------- #
+# The 'ContentType' depends on the requirements of the API.
+# Some require a JSON with the 'ContentType' set to: 'application/json' while others require
+# a form with the 'ContentType' set to: 'application/x-www-form-urlencoded'.
+# The 'body' might also require a 'username/password and grant_type' and, if the 'ContentType'
+# is 'application/json', it must also be converted to JSON. 
+# -------------------------------------------------------------------------------------------- #
 $splatRetrieveTokenParams = @{
     Uri         = ''
     Method      = 'POST'
-    ContentType = 'application/json'
+    ContentType = ''
     Body = @{
         client_id     = $($actionContext.Configuration.ClientId)
         client_secret = $($actionContext.Configuration.ClientSecret)
-    } | ConvertTo-Json
+    }
 }
 $responseToken = Invoke-RestMethod @splatRetrieveTokenParams
 
@@ -19,4 +24,4 @@ $splatRestParams = @{
     }
 }
 Invoke-RestMethod @splatRestParams
-# ---------------------------- #
+# -------------------------------------------------------------------------------------------- #
